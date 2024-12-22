a320dfe818419d570f0089670703a079f18a61af
hemk.dev <hemk.dev@gmail.com>
2024-12-22 23:20:38 +0530

    fonts utils changed

--- src/utility/fonts-utility.js
+++ src/utility/fonts-utility.js
@@ -1,12 +1,13 @@
 import { Lato, Playfair } from 'next/font/google'
+
 export const lato = Lato({
-    subsets: ['sans-serif'],
-    weight: ['300','400','700'],
-    preload: true
-})
+  subsets: ['latin'],  // Use 'latin' or 'latin-ext' instead of 'sans-serif'
+  weight: ['300', '400', '700'],
+  preload: true
+});
 
 export const playFair = Playfair({
-    subsets: ['sans-serif'],
-    weight: ['300','400','700'],
-    preload: true
-})
+  subsets: ['latin'],  // Use 'latin' or 'latin-ext' instead of 'sans-serif'
+  weight: ['300', '400', '700'],
+  preload: true
+});
